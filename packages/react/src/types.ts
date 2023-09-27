d119d9683d7aac90e6624789cab70afe5a6b0b67
Jan Thijs <92784122+janthijs@users.noreply.github.com>
2023-09-27 11:10:30 +0200

    fix: add to react package and remote eslint

--- packages/react/src/types.ts
+++ packages/react/src/types.ts
@@ -4,6 +4,7 @@ export interface PiwikInstance {
   trackPageView: PiwikTracker['trackPageView']
   trackSiteSearch: PiwikTracker['trackSiteSearch']
   trackLink: PiwikTracker['trackLink']
+  trackDownload: PiwikTracker['trackDownload']
   pushInstruction: PiwikTracker['pushInstruction']
 }
 
@@ -14,3 +15,5 @@ export type TrackPageViewParams = types.TrackPageViewParams
 export type TrackSiteSearchParams = types.TrackSiteSearchParams
 
 export type TrackLinkParams = types.TrackLinkParams
+
+export type TrackDownloadParams = types.TrackDownloadParams
